91deaf17632eebd7e58db1a80be68157b96ef0ff
ValerioB88 <valerio.biscione@gmail.com>
2022-02-02 15:06:10 +0100

    Update submodule, add pipeline file

--- code/experiments/run_pipeline.py
+++ code/experiments/run_pipeline.py
@@ -1,7 +1,8 @@
-from experiments import difficult_env, easy_env
+from experiments import difficult_env, easy_env, fluctuating_env, easy_difficult_pops
 
-difficult_env.run(0)
-difficult_env.run(1)
 
-[difficult_env.run(s) for s in range(10)]
+# [difficult_env.run(s) for s in range(9, 10)]
 [easy_env.run(s) for s in range(10)]
+[fluctuating_env.run(s) for s in range(10)]
+
+[easy_difficult_pops.run(s) for s in range(10)]
