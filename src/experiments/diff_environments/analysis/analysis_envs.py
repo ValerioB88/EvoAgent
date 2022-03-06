f6e9111024b0261e7cd439ccde20473b63eed2db
ValerioB88 <valerio.biscione@gmail.com>
2022-03-06 17:29:24 +0100

    Add new figures for post

--- src/experiments/diff_environments/analysis/analysis_envs.py
+++ src/experiments/diff_environments/analysis/analysis_envs.py
@@ -37,6 +37,11 @@ plot('difficult_envS0')
 plot('easy_envS0')
 plot('fluctuating_envS0')
 
+folder = './docs/assets/experiments/diff_environments/'
+pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+plt.figure(1)
+plt.savefig(folder + 'num_agents.png', dpi=300)
 plt.figure(2)
-pathlib.Path('./docs/assets/experiments/envs').mkdir(parents=True, exist_ok=True)
-plt.savefig('./docs/assets/experiments/envs/agents_over_food.png', dpi=300)
+plt.savefig(folder + 'agents_over_food.png', dpi=300)
+plt.figure(3)
+plt.savefig(folder + 'num_food.png', dpi=300)
