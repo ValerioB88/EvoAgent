8fef1a76e07ffb59d139101065c47a264cca8d6d
ValerioB88 <valerio.biscione@gmail.com>
2022-03-06 16:34:12 +0100

    Added some figures

--- src/experiments/analysis/analysis_envs.py
+++ src/experiments/analysis/analysis_envs.py
@@ -1,3 +1,4 @@
+import pathlib
 import glob
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,12 +18,14 @@ def plot(env):
     plt.figure(1)
     plt.plot(df['step'], df['num_agents'], label=env)
     plt.legend()
+    plt.title('Tot Num. Agents')
 
     plt.figure(2)
     plt.plot(df['step'], df['num_agents']/df['num_food'], label=env)
     plt.legend()
-
+    plt.title('Num. Agents/Num. Food')
     plt.figure(3)
+
     plt.plot(df['step'], df['num_food'], label=env)
     plt.legend()
 
@@ -33,3 +36,7 @@ plt.close('all')
 plot('difficult_envS0')
 plot('easy_envS0')
 plot('fluctuating_envS0')
+
+plt.figure(2)
+pathlib.Path('./docs/assets/experiments/envs').mkdir(parents=True, exist_ok=True)
+plt.savefig('./docs/assets/experiments/envs/agents_over_food.png', dpi=300)
